7bfb197b1310d40b0b434f2afb988a8a98cc321f
Osmel00 <osmel.fauregonzalez@dci.education>
2023-05-21 21:09:48 +0200

    finish cookies clear

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
       secure: true,
       sameSite: "none",
       
-      maxAge: 1000 * 60 * 60 * 24 * 7, //One week
+      //maxAge: 1000 * 60 * 60 * 24 * 7, //One week
      
     },
   })
